ae49a24d3f97d2930c9dd6970dea69d4bdba5428
Mate Dolidze <mdoli17@freeuni.edu.ge>
2021-02-01 13:04:54 +0400

    Initial Commit

---
+++ WeatherApp-FinalProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp-FinalProject
+//
+//  Created by Matt Dolidze on 01.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
